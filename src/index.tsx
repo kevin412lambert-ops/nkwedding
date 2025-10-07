8e086b9b8d27b8d86aa177fe53b42601faeb4cdb
kevin412lambert-ops <kevin412lambert@gmail.com>
2025-10-07 10:53:16 -0500

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import WeddingWebsite from './wedding-website.tsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <WeddingWebsite />
+  </React.StrictMode>
+);
